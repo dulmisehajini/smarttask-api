33ae38ed8b98be64e869f7ccd48bb3adc51936f5
Dulmi Sehajinie <152590953+dulmisehajini@users.noreply.github.com>
2026-05-12 13:48:00 +0530

    initial project setup - Spring Boot SmartTask API

---
+++ src/main/java/com/smarttask/SmarttaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.smarttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttaskApiApplication.class, args);
+	}
+
+}
